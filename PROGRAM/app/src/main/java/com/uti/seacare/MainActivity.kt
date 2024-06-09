1ceb9fa969baf3a28dadc7a407b0791550097e42
Claraindriani1764 <claraindriani1764@gmail.com>
2024-06-09 14:19:17 +0700

    clara : menampilkan fragment home jika membuat aplikasi

--- PROGRAM/app/src/main/java/com/uti/seacare/MainActivity.kt
+++ PROGRAM/app/src/main/java/com/uti/seacare/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(HomeFragment())
 
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId){
